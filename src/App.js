aeaaa84f8bf572f680bc489214e52ba59f3eee26
Mohsin Siddique <m.mohsinsiddique786@gmail.com>
2020-06-07 01:02:37 +0500

    Update App.js
    Deleted auto generated text and added custom text.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Basic React App
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React App deployment on Surge via Github Actions CICD.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
